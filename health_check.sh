b10e83f80026c94257a53423be738a3d43300d8b
Samuel Kaiponen <samuel.kaiponen@gmail.com>
2022-11-07 17:33:17 +0200

    Try script health check again

--- health_check.sh
+++ health_check.sh
@@ -3,7 +3,9 @@
 result="$(curl -s 'https://old-firefly-9065.fly.dev/health')"
 if [ "$result" == "ok" ]
 then
+    echo "Successfully received 'ok' from /health"
     exit 0
 else
+    echo "Failed to receive 'ok' from /health"
     exit 1
 fi
